ff6c803d72e4d63bf670e35971bcf1abc68ba878
Ayman Mustafa <mswain6@saddleback.edu>
2023-11-28 13:02:13 +0200

    add clerk auth

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,27 @@
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 import createMiddleware from "next-intl/middleware";
+import { NextResponse } from "next/server";
 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ["en", "ar", "es"],
-
-  // Used when no locale matches
+const intlMiddleware = createMiddleware({
+  locales: ["ar", "en", "es"],
   defaultLocale: "en",
-  localePrefix: "as-needed",
-  //   localePrefix: "never",
+});
+
+export default authMiddleware({
+  beforeAuth(request) {
+    return intlMiddleware(request);
+  },
+
+  // Ensure that locale-specific sign in pages are public
+  publicRoutes: ["/", "/:locale", "/:locale/sign-in", "/:locale/sign-up"],
 });
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ["/", "/(ar|en|es)/:path*"],
+
+  matcher: [
+    "/",
+    "/(ar|en|es)/:path*",
+    "/((?!api|_next/static|_next/image|favicon.ico|apple-touch-icon.png|favicon.svg|images/books|icons|manifest).*)",
+  ],
 };
